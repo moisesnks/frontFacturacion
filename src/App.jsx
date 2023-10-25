35c2f7933eb78212be8fb756333e1f7bc4e09e0e
lukas <lukas_skyry_14@hotmail.cl>
2023-10-25 18:54:44 -0300

    intentoWebpay

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+//import './App.css'
 import Compra from './components/Compra'
 import Hospedaje from './components/Hospedaje'
 import Pasajero from './components/Pasajero'
@@ -9,44 +9,109 @@ import Pagar2 from './components/Pagar2'
 import Footer from './components/Footer'
 import Button from 'react-bootstrap/Button'
 import reservaMock from './mocks/reserva'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   const handleBuscarClick = async () => {
     console.log(reservaMock);
+  }
+
+
+  const [formData, setFormData] = useState({
+    OrdenID: '',
+    SessionID: '',
+    Monto: 0,
+    UrlRetorno: '',
+  })
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+    setFormData({
+      OrdenID: "345ab",
+      SessionID: "1234",
+      Monto: costoTotal,
+      UrlRetorno: "http://localhost:3000/"
+    })
+
+
+    try {
+      const response = await fetch('http://localhost:8080/save-transaction', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Maneja la respuesta exitosa, si es necesario
+        console.log('Transacción guardada con éxito');
+      } else {
+        // Maneja errores
+        console.error('Error al guardar la transacción');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud', error);
     }
+  };
+
+
+
+  // Calcula el costo total del vuelo
+  const costoVuelo = reservaMock.precio_viaje;
+
+  // Calcula el costo total de la estancia en el hotel
+  const fechaIda = new Date(reservaMock.vuelo_info.fecha_ida);
+  const fechaVuelta = new Date(reservaMock.vuelo_info.fecha_vuelta);
+  const numeroDias = (fechaVuelta - fechaIda) / (1000 * 60 * 60 * 24); // Calcula el número de días
+  const precioNoche = reservaMock.info_paquete.precio_noche;
+  const costoEstanciaHotel = precioNoche * numeroDias;
+
+  // Calcula el costo total
+  const costoTotal = costoVuelo + costoEstanciaHotel;
+
+  console.log(formData)
+
+
+
+
   return (
     <>
-      <div className="container font">
-      <div className="row">
+      <div className="">
+        <NavBar></NavBar>
+        <div className="row">
           <div className="col-md-6 left">
             <Pagar2></Pagar2>
             <Pasajero></Pasajero>
-            
-            
+
+
           </div>
           <div className="col-md-6 rigth">
-            <TotalPago objeto={reservaMock} />
+            <TotalPago costoTotal={costoTotal} />
             <Compra objeto={reservaMock} />
             <Servicio></Servicio>
             <Hospedaje objeto={reservaMock} />
 
             <div className="btn">
 
-            <Button className="buscar" variant="warning" onClick={handleBuscarClick}>
-            CONFIRMAR DATOS INGRESADOS
-           </Button>
+              <Button className="buscar" variant="warning" onClick={handleSubmit}>
+                CONFIRMAR DATOS INGRESADOS
+              </Button>
+
+            </div>
+
+
+
+          </div>
+        </div>
 
       </div>
-            
-            
-            
-          </div>
-      </div>
-      
-    </div>
-    <div className='container-footer'>
+      <div>
         <Footer />
       </div>
     </>
--- src/components/TotalPago.jsx
+++ src/components/TotalPago.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import './totalPago.css';
 
-function TotalPago({ objeto }) {
-  // Calcula el costo total del vuelo
-  const costoVuelo = objeto.precio_viaje;
+function TotalPago({ costoTotal }) {
 
-  // Calcula el costo total de la estancia en el hotel
-  const fechaIda = new Date(objeto.vuelo_info.fecha_ida);
-  const fechaVuelta = new Date(objeto.vuelo_info.fecha_vuelta);
-  const numeroDias = (fechaVuelta - fechaIda) / (1000 * 60 * 60 * 24); // Calcula el número de días
-  const precioNoche = objeto.info_paquete.precio_noche;
-  const costoEstanciaHotel = precioNoche * numeroDias;
-
-  // Calcula el costo total
-  const costoTotal = costoVuelo + costoEstanciaHotel;
 
   return (
     <div className="row total m-3 shadow rounded">
